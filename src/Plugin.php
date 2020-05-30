4562dbd4ef81cd905b5716c631c7d8c7e14b3343
Mohammed S Shurrab <mshurrab@extreme.sa>
2020-05-30 12:56:52 +0300

    init

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,12 +2,52 @@
 
 declare(strict_types=1);
 
-namespace Pest\PluginName;
+namespace Devmsh\PestSmartRunner;
+
+use Pest\Support\Str;
+use Pest\TestSuite;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * @internal
  */
 final class Plugin
 {
-    // ..
+    /**
+     * @var string
+     */
+    private const CLASS_OPTION = 'class';
+
+    public $class = null;
+
+    public function handleArguments(TestSuite $testSuite, array $originals): array
+    {
+        $arguments = array_merge([''], array_values(array_filter($originals, function ($original): bool {
+            foreach ([self::CLASS_OPTION] as $option) {
+                if ($original === sprintf('--%s', $option) || Str::startsWith($original, sprintf('--%s=', $option))) {
+                    return true;
+                }
+            }
+
+            return false;
+        })));
+
+        $originals = array_flip($originals);
+        foreach ($arguments as $argument) {
+            unset($originals[$argument]);
+        }
+        $originals = array_flip($originals);
+
+        $inputs   = [];
+        $inputs[] = new InputOption(self::CLASS_OPTION, 'c', InputOption::VALUE_REQUIRED);
+
+        $input = new ArgvInput($arguments, new InputDefinition($inputs));
+        if ($input->getOption(self::CLASS_OPTION)) {
+            $this->class = $input->getOption(self::CLASS_OPTION);
+        }
+
+        return $originals;
+    }
 }
--- tests/Plugin.php
+++ tests/Plugin.php
@@ -1,7 +1,22 @@
 <?php
 
-use Pest\PluginName\Plugin;
+use Devmsh\PestSmartRunner\Plugin;
+use Pest\TestSuite;
 
 it('has plugin', function () {
     assertTrue(class_exists(Plugin::class));
+});
+
+it('supports run related class tests if --class exist', function () {
+    $plugin = new Plugin();
+    $testSuite = TestSuite::getInstance();
+
+    assertNull($plugin->class);
+    $arguments = $plugin->handleArguments($testSuite, []);
+    assertEquals([], $arguments);
+    assertNull($plugin->class);
+
+    $arguments = $plugin->handleArguments($testSuite, ['--class=App\\TestClass']);
+    assertEquals([], $arguments);
+    assertEquals($plugin->class, 'App\\TestClass');
 });
